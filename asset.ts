0910b956d4f0a708f6facd8e65b9c925a781513a
Luke Shaffer <lukeshaffer123@gmail.com>
2026-03-25 20:54:28 -1000

    Added more asset code

--- asset.ts
+++ asset.ts
@@ -85,37 +85,20 @@ export class Asset {
         this.shippingLegs = shippingLegs;
     }
 
-    // Just function signatures for now
+    /**
+     * TODO: Stub function, will fill in later. Might be able to merge with
+     * getCurrentState()
+     */
+    getCurrentLocation() {};
 
+    /**
+     * TODO: Stub function, will fill in later
+     */
+    getCurrentState() {};
 
-    /* === Private Methods === */
-
-
-
-    /* === Public Methods === */
-
-    function setAssetTolerances(uint tempMax, uint tempMin,
-        uint humMax, uint humMin, uint maxForce
-    )
-
-    // Add a new shipping address onto this asset's lifespan
-    // shipping locations can only be added in order and require a maximum time
-    // in MS to be delivered from their previous location, after which the asset
-    // will be declared defacto damaged even if no other damage has occurred
-    function addAssetShippingLocation(address shippingLocation, uint maxShippingTimeMs) public
-    
-    // chron-type checkup function that reads asset sensors and will mark the
-    // asset damaged if any of the damage tracking stats exceeds their total
-    // Also implicitly checks if the current delivery time has exceeded the
-    //  maximum allowable for this leg of the delivery.
-    // - temp is a temperature, specified in degrees F * 100 for decimals
-    // - humidity is specified as a percentage from 100 * 100 for decimals
-    // - maxForce represents an accelerometer reading for the object in units
-    //      of g * 100. Seems a normal range is to check up to 16 g
-    function assessDamage(uint temp, uint humidity, uint maxForce) public
-
-    // Public function to be called when the asset is within the geographic
-    // bounds of the delivery point. Transitions the asset to the next
-    // ownership phase
-    function deliverAsset() public
+    getCurrentDeliveryReceiver() {
+        this.shippingLegs.forEach(leg => {
+            if (!leg.isComplete) return leg.shippingReceiver;
+        })
+    }
 }
--- assetTransfer.ts
+++ assetTransfer.ts
@@ -9,6 +9,7 @@ import {Asset, ShippingLeg, ShippingTolerances} from './asset';
 
 type PartialWithRequired<T, K extends keyof T> = Partial<T> & Required<Pick<T, K>>;
 
+// This class contains all of the asset handling and governance logic.
 @Info({title: 'AssetTransfer', description: 'Smart contract for IoT asset tracking and shipping logistics'});
 export class AssetTransferContract extends Contract {
 
@@ -24,7 +25,7 @@ export class AssetTransferContract extends Contract {
 
     @Transaction(false)
     @Returns('boolean')
-    public async AssetExists(ctx: Context, id: string|null|undefined) {
+    public async AssetExists(ctx: Context, id: string|undefined) {
         if (id === null || id === undefined) return false;
         const assetJSON = await ctx.stub.getState(id);
         return assetJSON.length > 0;
@@ -32,7 +33,7 @@ export class AssetTransferContract extends Contract {
 
     // Get an asset from its id
     @Transaction(false)
-    public async ReadAsset(ctx: Context, id: string): Promise<string> {
+    public async ReadAsset(ctx: Context, id: string|undefined): Promise<string> {
         // somehow magically gets the asset from the current context
         const assetJSON = await ctx.stub.getState(id);
 
@@ -56,26 +57,98 @@ export class AssetTransferContract extends Contract {
         this.updateAsset(ctx, asset);
     }
 
+
+    /**
+     * Add a shipping leg to this asset's planned route. Throws when the asset
+     * specified has already left the warehouse
+     * @param ctx - The hyperledger context
+     * @param assetId - The id of the asset you want to edit
+     * @param shippingLeg - The new shipping details
+     */
     @Transaction()
     public async addShippingLeg(ctx: Context, assetId: string, shippingLeg: ShippingLeg): Promise<void> {
         let asset: Asset = new Asset(JSON.parse(await this.ReadAsset(ctx, assetId)));
 
+        if (asset.isShipped || asset.isDelivered) {
+            throw new Error(`Error, can't edit an asset that has already been shipped`);
+        }
+
         // TODO: check if user adding shipping leg is authorized
+        
 
         asset.shippingLegs.push(shippingLeg);
         this.updateAsset(ctx, asset)
 
     }
 
-    // Partial<T> is a built-in that does what it sounds - does not require all
-    // fields
+    /**
+     * Update the asset with a partial mapping of new attribute values
+     * @param ctx - The hyperledger context
+     * @param newAsset - A simple Object with only a required "assetId" key
+     * @returns 
+     */
     @Transaction()
     public async updateAsset(ctx: Context, newAsset: PartialWithRequired<Asset, 'assetId'>): Promise<void> {
-        let existing = this.ReadAsset(ctx, newAsset.assetId)
+        let existing = new Asset(JSON.parse(await this.ReadAsset(ctx, newAsset.assetId)));
         
         // takes left and updates it with all the values present in right
         let updated = Object.assign(existing, newAsset);
         
         return ctx.stub.putState(updated.assetId, Buffer.from(stringify(sortKeysRecursive(updated))));
+    }
+     
+   /**
+    * Assess the current state of an asset and return a boolean of whether it
+    * is bad enough to qualify as damaged.
+    * @param ctx - The hyperledger context
+    * @param id - The id of the asset
+    * @returns 
+    */
+    @Transaction()
+    public async assessDamage(ctx: Context, id: string|undefined): Promise<boolean> {
+        let asset = new Asset(JSON.parse(await this.ReadAsset(ctx, id)));
+
+        // Haven't fully defined how we will be assessing the current state of
+        // the asset, temp, location, etc
+
+        // leaving this as a mock, this is a TODO item
+        let assetStats:Map<string, number> = asset.getCurrentState();
+        if (!assetStats) return true;
+
+        for (const [tolName, tolVal] of Object.entries(asset.shippingTolerances)){
+            if (tolName.includes('Min') && assetStats.get(tolName) !== undefined) {
+                if (tolVal > assetStats.get(tolName)) return true;
+            }
+            else if (tolName.includes('Max') && assetStats.get(tolName) !== undefined) {
+                if (tolVal < assetStats.get(tolName)) return true;
+            }
+        }
+        return false;
+    }
+
+
+    @Transaction()
+    public async transferAsset(ctx: Context, id:string|undefined): Promise<void> {
+        const asset = new Asset(JSON.parse(await this.ReadAsset(ctx, id)));
+
+        // TODO, hasn't been implemented because I don't know where we're storing
+        // the data
+        const assetCurrentLocation = asset.getCurrentLocation();
+        const assetReceiver = asset.getCurrentDeliveryReceiver()
+
+        // TODO, not sure how I am going to link delivery party identities with
+        // an immutable delivery location, but that is what I am looking to do
+        // here
+        const DELIVERY_LOCATIONS = {};
+        if (isWithin(DELIVERY_LOCATIONS[assetReceiver], assetCurrentLocation)){
+            // TODO - this will need to be fleshed out better
+
+            // Mark shipping leg complete, and transition on to the next one.
+            // If there is no next shipping leg, mark the asset as successfully
+            // finally delivered
+
+            // Finally save the updated asset
+        }
+        
     }
 }
